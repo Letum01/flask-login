f0bb81565673f2802ec9147204d6de81346eb8aa
Thắng Lê <lenguyenhuuthang283@gmail.com>
2026-03-16 00:30:05 +0700

    finish CRUD items

--- app.py
+++ app.py
@@ -9,12 +9,18 @@ def get_db():
     return sqlite3.connect("database.db")
 
 
+# ================= HOME =================
+
 @app.route("/")
 def home():
+
     if "user" in session:
-        return "Hello " + session["user"]
+        return redirect("/items")
+
     return redirect("/login")
 
+
+# ================= REGISTER =================
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -25,16 +31,20 @@ def register():
         password = request.form["password"]
 
         db = get_db()
+
         db.execute(
             "INSERT INTO users (user,password) VALUES (?,?)",
             (user, password)
         )
+
         db.commit()
 
         return redirect("/login")
 
     return render_template("register.html")
 
+
+# ================= LOGIN =================
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -55,15 +65,78 @@ def login():
 
         if data:
             session["user"] = user
-            return redirect("/")
+            return redirect("/items")
 
     return render_template("login.html")
 
 
+# ================= LOGOUT =================
+
 @app.route("/logout")
 def logout():
+
     session.clear()
+
     return redirect("/login")
+
+
+# ================= ITEMS =================
+
+@app.route("/items")
+def items():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    db = get_db()
+
+    cur = db.execute("SELECT * FROM items")
+
+    data = cur.fetchall()
+
+    return render_template("items.html", data=data)
+
+
+# ================= ADD =================
+
+@app.route("/add", methods=["POST"])
+def add():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    name = request.form["name"]
+
+    db = get_db()
+
+    db.execute(
+        "INSERT INTO items(name) VALUES (?)",
+        (name,)
+    )
+
+    db.commit()
+
+    return redirect("/items")
+
+
+# ================= DELETE =================
+
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    if "user" not in session:
+        return redirect("/login")
+
+    db = get_db()
+
+    db.execute(
+        "DELETE FROM items WHERE id=?",
+        (id,)
+    )
+
+    db.commit()
+
+    return redirect("/items")
 
 
 if __name__ == "__main__":
